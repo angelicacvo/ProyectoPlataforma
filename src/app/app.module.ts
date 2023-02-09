740834aa71377e6dca3a17476040e73fc89ac888
angelicacvo <angiemarin0707@gmail.com>
2023-02-08 22:34:57 -0500

    creación componente ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { YogaClientComponent } from './body/yoga-client/yoga-client.component';
 import { NutritionClientComponent } from './body/nutrition-client/nutrition-client.component';
-import { CoursesEcommerceComponent } from './body/courses-ecommerce/courses-ecommerce.component';
 import { RecetarioNutritionComponent } from './body/nutrition-client/recetario-nutrition/recetario-nutrition.component';
 import { RecetasNutritionComponent } from './body/nutrition-client/recetas-nutrition/recetas-nutrition.component';
 import { ContactNutritionComponent } from './body/nutrition-client/contact-nutrition/contact-nutrition.component';
@@ -15,6 +14,7 @@ import { RegisterComponent } from './body/register/register.component';
 import { ServicesComponent } from './body/services/services.component';
 import { HomeClientComponent } from './body/home-client/home-client.component';
 import { HomeaboutusComponent } from './body/homeaboutus/homeaboutus.component';
+import { EcommerceComponent } from './body/ecommerce/ecommerce.component';
 
 
 @NgModule({
@@ -25,14 +25,14 @@ import { HomeaboutusComponent } from './body/homeaboutus/homeaboutus.component';
     BodyComponent,
     YogaClientComponent,
     NutritionClientComponent,
-    CoursesEcommerceComponent,
     RecetarioNutritionComponent,
     ContactNutritionComponent,
     RegisterComponent,
     ServicesComponent,
     HomeClientComponent,
     HomeaboutusComponent,
-    RecetasNutritionComponent
+    RecetasNutritionComponent,
+    EcommerceComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
